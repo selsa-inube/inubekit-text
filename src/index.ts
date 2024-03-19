6a01bf3beb5b70caf39f7db6eb9762e706eb001f
cmarin001 <cmarin6051@gmail.com>
2024-03-19 18:00:40 -0500

    fix(text): remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { IText } from "./Text";
-import {
+export { Text } from "./Text";
+export type { IText } from "./Text";
+export type {
   ITextAlignment,
   ITextAppearance,
   ITextHtmlElement,
   ITextSize,
   ITextType,
 } from "./Text/props";
-
-export { Text } from "./Text";
-export type {
-  IText,
-  ITextAlignment,
-  ITextAppearance,
-  ITextHtmlElement,
-  ITextSize,
-  ITextType,
-};
